ed22cf9adcabbd2bb5e1186ff99916063a3561a5
tomggill <tom.gill25@outlook.com>
2025-01-14 20:17:51 +0000

    refactor: Refactor auth handler to use database instead of insufficient MemoryStore

--- src/route.rs
+++ src/route.rs
@@ -1,12 +1,15 @@
 use axum::{routing::get, Router};
 
-use crate::{google_auth, index, login_authorized, logout, protected, AppState};
+use crate::{
+    handler::auth_handler::{auth_callback, google_auth},
+    index, logout, protected, AppState,
+};
 
 pub async fn create_router(app_state: AppState) -> Router {
     Router::new()
         .route("/", get(index))
         .route("/auth/google", get(google_auth))
-        .route("/auth/authorized", get(login_authorized))
+        .route("/auth/authorized", get(auth_callback))
         .route("/protected", get(protected))
         .route("/logout", get(logout))
         .with_state(app_state)
