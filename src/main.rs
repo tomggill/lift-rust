aa487db32575ed70f95bfcd7f7130594398d2c67
tomggill <tom.gill25@outlook.com>
2025-01-18 16:38:36 +0000

    feat: Create google token service and refactor auth functionality

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-mod client;
 mod config;
 mod errors;
 mod handler;
 mod middleware;
 mod route;
+mod service;
 
 use anyhow::{Context, Result};
-use async_session::{MemoryStore, SessionStore};
 use axum::{
     extract::{FromRef, State},
     response::{IntoResponse, Redirect, Response},
@@ -15,17 +14,15 @@ use axum_extra::{extract::cookie::Cookie, headers, TypedHeader};
 use dotenv::dotenv;
 use errors::AppError;
 use http::{header::SET_COOKIE, HeaderMap, Method};
-use oauth2::{basic::BasicClient, reqwest::async_http_client, AccessToken, AuthUrl, ClientId, ClientSecret, RedirectUrl, RevocableToken, RevocationUrl, StandardRevocableToken, TokenUrl};
 use reqwest::Client;
 use route::create_router;
 use serde::{Deserialize, Serialize};
+use service::google_token_service::{GoogleTokenService, TokenServiceTrait};
 use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 use tokio::sync::RwLock;
 use std::{env, sync::Arc};
 use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-static COOKIE_NAME: &str = "SESSION";
 
 #[derive(Clone)]
 struct UserContext {
@@ -37,16 +34,15 @@ struct UserContext {
 
 #[derive(Clone)]
 struct AppState {
-    oauth_client: BasicClient,
     http_client: Client,
     db: MySqlPool,
     user_context: Arc<RwLock<Option<UserContext>>>,
-
+    google_token_service: GoogleTokenService,
 }
 
-impl FromRef<AppState> for BasicClient {
+impl FromRef<AppState> for GoogleTokenService {
     fn from_ref(state: &AppState) -> Self {
-        state.oauth_client.clone()
+        state.google_token_service.clone()
     }
 }
 
@@ -61,15 +57,13 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    // `MemoryStore` is just used as an example. Don't use this in production.
-    let oauth_client = oauth_client().unwrap();
     let db = get_sql_pool().await.unwrap();
     let http_client = Client::new();
     let app_state = AppState {
-        oauth_client,
         db,
         http_client,
         user_context: Arc::new(RwLock::new(None)),
+        google_token_service: GoogleTokenService::new(),
     };
 
     let cors = CorsLayer::new()
@@ -106,37 +100,6 @@ async fn get_sql_pool() -> Result<MySqlPool, AppError> {
     Ok(pool)
 }
 
-fn oauth_client() -> Result<BasicClient, AppError> {
-    let client_id = std::env::var("GOOGLE_CLIENT_ID").context("Missing CLIENT_ID!")?;
-    let client_secret = std::env::var("GOOGLE_CLIENT_SECRET").context("Missing CLIENT_SECRET!")?;
-    let redirect_url = std::env::var("GOOGLE_REDIRECT_URL")
-        .unwrap_or_else(|_| "http://127.0.0.1:3000/auth/authorized".to_string());
-
-    let auth_url = std::env::var("GOOGLE_AUTH_URL")
-        .unwrap_or_else(|_| "https://accounts.google.com/o/oauth2/v2/auth".to_string());
-
-    let token_url = std::env::var("GOOGLE_TOKEN_URL")
-        .unwrap_or_else(|_| "https://oauth2.googleapis.com/token".to_string());
-
-    let revocation_url = std::env::var("GOOGLE_REVOCATION_URL")
-        .unwrap_or_else(|_| "https://oauth2.googleapis.com/revoke".to_string());
-
-
-    Ok(BasicClient::new(
-        ClientId::new(client_id),
-        Some(ClientSecret::new(client_secret)),
-        AuthUrl::new(auth_url).context("failed to create new authorization server URL")?,
-        Some(TokenUrl::new(token_url).context("failed to create new token endpoint URL")?),
-    )
-    .set_redirect_uri(
-        RedirectUrl::new(redirect_url).context("failed to create new redirection URL")?,
-    )
-    .set_revocation_uri(
-        RevocationUrl::new(revocation_url).context("failed to create new revocation URL")?,
-    )
-    )
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
     sub: String,
@@ -162,60 +125,6 @@ async fn protected(State(app_state): State<AppState>) -> Result<impl IntoRespons
         Some(user) => Ok(format!("Welcome to the protected area, {}!", user.name)),
         None => Err(anyhow::anyhow!("You're not logged in.").into()),
     }
-}
-
-async fn logout(
-    State(app_state): State<AppState>,
-    State(oauth_client): State<BasicClient>,
-    TypedHeader(cookies): TypedHeader<headers::Cookie>,
-) -> Result<impl IntoResponse, AppError> {
-    // TODO: Revocation of access and refresh token not necessary as revoking a refresh 
-    // token in Google OAUTH 2.0 also revokes the associated access token and vice versa.
-    // See: https://cloud.google.com/apigee/docs/api-platform/security/oauth/validating-and-invalidating-access-tokens
-    if let Some(refresh_token) = cookies.get("refresh_token") {
-        revoke_token(refresh_token.to_string(), &oauth_client).await?;
-    }
-
-    {
-        let mut user_context_lock = app_state.user_context.write().await;
-        *user_context_lock = None;
-    }
-
-    // TODO - refactor
-    let empty_access_token = Cookie::build(("access_token", ""))
-        .path("/")
-        .http_only(true)
-        .build();
-    let empty_refresh_token = Cookie::build(("refresh_token", ""))
-        .path("/")
-        .http_only(true)
-        .build();
-
-    let mut headers = HeaderMap::new();
-    headers.append(
-        SET_COOKIE,
-        empty_access_token.to_string().parse().unwrap(),
-    );
-    headers.append(
-        SET_COOKIE,
-        empty_refresh_token.to_string().parse().unwrap(),
-    );
-
-
-    // Redirect to the home page
-    Ok((headers, Redirect::to("/")))
-}
-
-async fn revoke_token(token: String, oauth_client: &BasicClient) -> Result<(), AppError> {
-    let token = AccessToken::new(token);
-    let revocable_token: StandardRevocableToken = token.into();
-
-    let response = oauth_client.revoke_token(revocable_token)?.request_async(async_http_client).await;
-
-    if let Err(error) = response {
-        return Err(AppError::from(error));
-    } 
-    Ok(())
 }
 
 #[derive(Debug, Deserialize)]
--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,4 +1,3 @@
-use anyhow::Context;
 use axum::{
     extract::{Request, State},
     middleware::Next,
@@ -8,39 +7,36 @@ use axum::{
 
 use axum_extra::extract::cookie::{Cookie, CookieJar};
 
-use oauth2::{
-    basic::BasicClient, reqwest::async_http_client, AccessToken, RefreshToken, TokenResponse,
-};
-
-use crate::{client::google_client::GoogleClient, errors::AppError, handler::auth_handler::get_user, AppState, UserContext};
+use crate::{ errors::AppError, handler::auth_handler::get_user, service::google_token_service::{GoogleTokenService, TokenServiceTrait}, AppState, UserContext};
 
 // TODO - Add appropriate error responses
 pub async fn auth(
     State(app_state): State<AppState>,
+    State(google_token_service): State<GoogleTokenService>,
     mut req: Request,
     next: Next,
 ) -> Result<impl IntoResponse, AppError> {
-    let cookies = CookieJar::from_headers(&req.headers());
+    let cookies = CookieJar::from_headers(req.headers());
     if let Some(access_token_cookie) = cookies.get("access_token") {
         let access_token = access_token_cookie.value().to_string();
-        if let Some(_) = validate_and_set_user_context(&app_state, &access_token).await? {
+        if let Some(_) = validate_and_set_user_context(&app_state, &google_token_service, &access_token).await? {
             return Ok(next.run(req).await);
         }
     }
 
     if let Some(refresh_token_cookie) = cookies.get("refresh_token") {
         let refresh_token = refresh_token_cookie.value().to_string();
-        return handle_refresh_token(&app_state, &refresh_token, req, next).await;
+        return handle_refresh_token(&app_state, &google_token_service, &refresh_token, req, next).await;
     }
     Ok(Redirect::to("/").into_response())
 }
 
 async fn validate_and_set_user_context(
     app_state: &AppState,
+    google_token_service: &GoogleTokenService,
     access_token: &str,
 ) -> Result<Option<UserContext>, AppError> {
-    let google_client = GoogleClient::new(&app_state.http_client);
-    if let Ok(google_token_info) = google_client.validate_access_token(access_token).await {
+    if let Ok(google_token_info) = google_token_service.get_token_info(access_token).await {
         let existing_user = get_user(&google_token_info.user_id, app_state).await?;
         if let Some(user_context) = existing_user {
             let mut user_context_lock = app_state.user_context.write().await;
@@ -53,11 +49,13 @@ async fn validate_and_set_user_context(
 
 async fn handle_refresh_token(
     app_state: &AppState,
+    google_token_service: &GoogleTokenService,
     refresh_token: &str,
     req: Request,
     next: Next,
 ) -> Result<http::Response<axum::body::Body>, AppError> {
-    if let Ok(new_access_token) = refresh_access_token(refresh_token, &app_state.oauth_client).await {
+    if let Ok(new_access_token) = 
+    google_token_service.refresh_access_token(refresh_token.to_string()).await {
         let new_access_token_cookie = Cookie::build(("access_token", new_access_token.secret().to_string()))
             .path("/")
             .http_only(true)
@@ -67,22 +65,9 @@ async fn handle_refresh_token(
             SET_COOKIE,
             new_access_token_cookie.to_string().parse().unwrap(),
         );
-        if let Some(_) = validate_and_set_user_context(app_state, new_access_token.secret()).await? {
+        if let Some(_) = validate_and_set_user_context(app_state, google_token_service, new_access_token.secret()).await? {
             return Ok(response);
         }
     }
     Ok(Redirect::to("/").into_response())
-}
-
-async fn refresh_access_token(
-    refresh_token: &str,
-    client: &BasicClient,
-) -> Result<AccessToken, AppError> {
-    let token_response = client
-        .exchange_refresh_token(&RefreshToken::new(refresh_token.to_string()))
-        .request_async(async_http_client)
-        .await
-        .context("failed to refresh access token")?;
-
-    Ok(token_response.access_token().to_owned())
 }
